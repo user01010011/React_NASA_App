ed15941cb4d294e5e217aff88f74c808f5f1b5d9
Shasha Yan <shashayan@gmail.com>
2022-01-19 22:34:43 -0600

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"; 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <img style={{width: "640px", height:"640px", borderRadius:"80px"}} src="https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" className="App-photo" alt="app-photo" />
         <p>
           Spacestagram: Image-sharing from the final frontier 
@@ -19,6 +21,7 @@ function App() {
           Enter
         </a> */}
       </header>
+      
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react"; 
+
+const NavBar = () => {
+    return (
+        <div class="navbar"> 
+            <div>
+                <h2 class="nav-logo">
+                    Spacestagram 
+                </h2>
+            </div>
+            {/* <div class="navbar-links">
+                <a href="https://github.com/user01010011" target="_blank" rel="noreferrer" class="github">Github</a>
+                <a href="https://medium.com/@isabella.y" target="_blank" rel="noreferrer" class="blog">Blog</a>
+            </div> */}
+        </div>
+    )
+}
+
+export default NavBar;
