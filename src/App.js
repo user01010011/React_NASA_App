93d82033c59a4f07dda7fbc02208e14aa601d547
Shasha Yan <shashayan@gmail.com>
2022-01-19 21:47:37 -0600

    update app page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img style={{width: "640px", height:"640px", borderRadius:"80px"}} src="https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" className="App-photo" alt="app-photo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Spacestagram: Image-sharing from the final frontier 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter
         </a>
       </header>
     </div>
